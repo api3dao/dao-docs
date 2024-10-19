7a7060481e6e43b2a5d8ffafd76f505d5822022d
Warren Anderson <warren@api3.org>
2024-10-19 11:02:09 -0600

    first commit

---
+++ docs/members/sidebar.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    text: 'Overview',
+    link: '/dao/',
+  },
+
+  {
+    text: 'Members',
+    collapsed: false,
+    items: [
+      {
+        text: 'Overview',
+        link: '/dao/',
+      },
+    ],
+  },
+  {
+    text: 'Technical',
+    collapsed: true,
+    items: [
+      {
+        text: 'Contracts',
+        link: '/dao/',
+      },
+    ],
+  },
+];
